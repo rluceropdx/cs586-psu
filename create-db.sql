8e017c888c43570e9b3ef2ce8715834bd35dce74
Farani Lucero <farani.lucero@sentar.com>
2024-12-06 11:14:43 +0300

    insert updates

--- create-db.sql
+++ create-db.sql
@@ -92,33 +92,57 @@ alter table crew add foreign key (operates_flights_id) references operates_fligh
 
 
 
-insert into airline (name, icao_code, iata_code) values ('Delta', 'DL', 'DL');
+--insert into airline (name, icao_code, iata_code) values ('Delta', 'DL', 'DL');
+--select * from airline where name = 'Delta Air Lines'
 
-insert into city (name, country, population, time_zone) values ('Portland', 'USA', 2500000, 'Pacific');
-insert into city (name, country, population, time_zone) values ('Amsterdam', 'Netherlands', 2500000, 'Europe/Amsterdam');
+--insert into city (name, country, population, time_zone) values ('Portland', 'USA', 2500000, 'Pacific');
+--insert into city (name, country, population, time_zone) values ('Amsterdam', 'Netherlands', 2500000, 'Europe/Amsterdam');
+-- select id from city where name like '%Portland%' limit 1;
+--select id from city where name like '%Amsterdam%' limit 1;
 
-insert into airport (name, iata_code, is_international, terminal_count, gate_count)
-values ('Portland International Airport', 'PDX', 'T', 4, 25);
-insert into airport (name, iata_code, is_international, terminal_count, gate_count)
-values ('Schiphol International Airport', 'AMS', 'T', 8, 50);
+
+--insert into airport (name, iata_code, is_international, terminal_count, gate_count)
+--values ('Portland International Airport', 'PDX', 'T', 4, 25);
+--insert into airport (name, iata_code, is_international, terminal_count, gate_count)
+--values ('Schiphol International Airport', 'AMS', 'T', 8, 50);
+--select id from airport where name like '%John F Kennedy%'
+--select id from airport where name like '%Portland International Airport%'
 
 
 insert into employee (name, title, start_date, years_experience, airline_id)
-values ('Bob', 'Flight Attendant', '2024-01-01', 1, 1);
+select 'Bob', 'Flight Attendant', '2024-01-01', 1, id 
+from airline where name = 'Delta Air Lines';
+	   
 insert into employee (name, title, start_date, years_experience, airline_id)
-values ('Jane', 'Flight Attendant', '2024-01-01', 1, 1);
+select 'Jane', 'Flight Attendant', '2024-01-01', 1, id 
+from airline where name = 'Delta Air Lines';
+
 insert into employee (name, title, start_date, years_experience, airline_id)
-values ('Barbara', 'Pilot', '2010-01-01', 20, 1);
+select 'Barbara', 'Pilot', '2010-01-01', 20, id 
+from airline where name = 'Delta Air Lines';
+
 insert into employee (name, title, start_date, years_experience, airline_id)
-values ('Joe', 'Pilot', '2020-01-01', 10, 1);
+select 'Joe', 'Pilot', '2020-01-01', 10, id 
+from airline where name = 'Delta Air Lines';
 
 insert into crew (name) values ('crew1');
 
-insert into crew_staff (role, crew_id, employee_id) values ('FA', 1, 1);
-insert into crew_staff (role, crew_id, employee_id) values ('FA', 1, 2);
-insert into crew_staff (role, crew_id, employee_id) values ('Pilot', 1, 3);
-insert into crew_staff (role, crew_id, employee_id) values ('Pilot', 1, 4);
+insert into crew_staff (role, crew_id, employee_id) 
+select 'FA', 1, id
+from employee where name = 'Bob';
+
+insert into crew_staff (role, crew_id, employee_id) 
+select 'FA', 1, id
+from employee where name = 'Jane';
+
+insert into crew_staff (role, crew_id, employee_id)
+select 'Pilot', 1, id
+from employee where name = 'Barbara';
+
+insert into crew_staff (role, crew_id, employee_id)
+select 'Pilot', 1, id
+from employee where name = 'Joe';
 
 insert into flies_to_from (flight_num, depart_airport_id, arrive_airport_id, codeshare, days_sched, depart_time_sched, arrive_time_sched, airline_id)
-values ('DAL145', 1, 2, 'KLM145', 'MTWRF', '14:00:00', '10:00:00', 1);
-
+select 'DAL145', 1442, 1682, 'KLM145', 'MTWRF', '14:00:00', '10:00:00', id
+from airline where name = 'Delta Air Lines';
